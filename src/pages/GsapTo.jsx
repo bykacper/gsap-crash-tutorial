c8857f9ecd41f29bf65f2a93e9f950f9ff52ed03
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-12-15 21:22:33 +0530

    started learning gsap

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,5 +1,13 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
+  useGSAP(() => {
+    gsap.to("#blue-box", {
+      x: 350,
+    });
+  }, []);
 
   return (
     <main>
